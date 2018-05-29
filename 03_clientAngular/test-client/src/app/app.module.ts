d46558339f047acbbb9427da4b5993f7b9996ab5
i2118a7 <dalibor.dubajic.external@banque-france.fr>
2018-05-29 14:52:00 +0200

    03 nettoyé

--- 03_clientAngular/test-client/src/app/app.module.ts
+++ 03_clientAngular/test-client/src/app/app.module.ts
@@ -2,12 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { CoreComponent } from './core/core.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CoreComponent
   ],
   imports: [
     BrowserModule
